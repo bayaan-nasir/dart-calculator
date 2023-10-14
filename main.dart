d07e8eda4916d4ed61cd5d134af133a95fbf507a
Bayaan <65360444+bayaan-nasir@users.noreply.github.com>
2023-10-14 12:09:57 +0000

    Update main.dart

--- main.dart
+++ main.dart
@@ -3,5 +3,12 @@ import 'dart:io';
 void main() {
   stdout.writeln("Enter your first name: ");
   String? name = stdin.readLineSync();
-  print(" Hello $name");
+  print("Hello $name");
+
+  print(sum(2, 5));
+}
+
+int sum(int f, int l) {
+  int sum = f + l;
+  return sum;
 }
